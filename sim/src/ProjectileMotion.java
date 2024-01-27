bd17bdf1da39686ab266b539a08f2ca5adf80ec2
arjuncaputo <arjuncaputo2025@gmail.com>
2024-01-27 16:19:08 -0700

    sim base

--- sim/src/ProjectileMotion.java
+++ sim/src/ProjectileMotion.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class ProjectileMotion {
+package src;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class ProjectileMotion extends JPanel{
+
+    public Simulation physics = new Simulation();
+
+    public void initComponents(){
+        JFrame frame = new JFrame("Projectile Motion Simulation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(this);
+        frame.setVisible(true);
+        physics.setup();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        physics.draw(g2);
+    }
+
+    public static void main(String[] args){
+        new ProjectileMotion().initComponents();
+    }
 }
--- sim/src/Simulation.java
+++ sim/src/Simulation.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Simulation {
+package src;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+
+public class Simulation{
+
+    private double x;
+    private double y;
+    private double initialX;
+    private double initialY;
+    private double angle;
+    private double velocity;
+    private double xVelocity;
+    private double yVelocity;
+    private double time;
+    private final double gravity = 9.81;
+
+    public void setup(){
+        x = 0;
+        y = 400;
+        initialX = x;
+        initialY = y;
+        angle = 45;
+        velocity = 80;
+        xVelocity = velocity * Math.cos(Math.toRadians(angle));
+        yVelocity = velocity * Math.sin(Math.toRadians(angle));
+        time = 0;
+    }
+
+    public void draw(Graphics2D g){
+        for(int i = 0; i < 1000; i++){
+            g.fill(new Ellipse2D.Double(x, y, 5, 5));
+            time += 0.1;
+            x = initialX + xVelocity * time;
+            y = initialY - (yVelocity * time - (gravity / 2) * time * time);
+
+        }
+
+        g.draw(new Line2D.Double(1, 5, 1, 400));
+    }
 }

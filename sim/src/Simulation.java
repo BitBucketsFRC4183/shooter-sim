c171fbfb27c0879390b8d3bfb10b6c8be1390a5c
Scott Song <ScottSong2020@gmail.com>
2024-02-03 19:31:22 -0700

    i lied again another commit to find optimal veloity and angle without calculus

--- sim/src/Simulation.java
+++ sim/src/Simulation.java
@@ -18,20 +18,33 @@ public class Simulation {
     double y;
     double holdX;
     double holdY;
+    double slope;
     int goodPath;
+    int goodGoodPath;
     int badPath;
     int numCurves;
+    int numVelocities;
     double Vxi;
     double Vyi;
     double Ax;
     double Ay;
     double initialX;
     double initialY;
+
     double theta;
+    double initialTheta;
+    double finalTheta;
     List<Double> thetaList = new ArrayList<>();
     double delta_theta;
     double bestTheta;
+
     double velocity;
+    double initialVelocity;
+    double finalVelocity;
+    List<Double> velocityList = new ArrayList<>();
+    double delta_velocity;
+    double bestVelocity;
+
     double time;
     final double gravity = 386.1;
     double dt;
@@ -55,26 +68,35 @@ public class Simulation {
         area_cross_section = 0.4;
         note_radius = 0.1778;
         air_density = 1.225;
-        x = 0; //change,0
-        y = 400; //400
+        x = 0; // get from vision
+        y = 400; // get from vision
         initialX = 0; //starting X of shooter
         initialY = 400; //starting Y of shooter
         goodPath = 0;
+        goodGoodPath = 0;
         badPath = 0;
         bestTheta = 0;
 
         //tune
         drag_coefficient = 0;
         magnus_coefficient = 0;
-        theta = 15;
-        delta_theta = 8;
-        numCurves = 8; //8
-        velocity = 45;
+        numCurves = 60; //8
+        numVelocities = 1000;
+
+        initialTheta = 15;
+        finalTheta = 75;
+        theta = initialTheta;
+        delta_theta = (finalTheta - initialTheta)/numCurves;
+
+        initialVelocity = 100; //170
+        finalVelocity = 900; //340
+        velocity = initialVelocity;
+        delta_velocity = (finalVelocity - initialVelocity)/numVelocities;
+
         Vxi = getVxi(theta);
         Vyi = getVyi(theta);
         time = 0;
-        dt = 0.02; //0.02
-
+        dt = 0.001; //0.02
     }
 
     public void resetSetup()
@@ -127,38 +149,53 @@ public class Simulation {
 
 
     public void drawOneCurve(Graphics2D g) {
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000; i++) {
             g.fill(new Ellipse2D.Double(x, y, 1, 1));
             time += dt;
             holdX = x;
             holdY = y;
             updateVelocityAndAcceleration();
             Line2D newLine = new Line2D.Double(holdX, holdY, x, y);
-            if (newLine.intersectsLine(frontSpeaker) || newLine.intersectsLine(topSpeaker) || newLine.intersectsLine(backSpeaker)) {
+            slope = (y - holdY)/(x - holdX);
+            if (newLine.intersectsLine(frontSpeaker) || newLine.intersectsLine(topSpeaker) || newLine.intersectsLine(backSpeaker) || (newLine.intersectsLine(backLine) && slope <= 0)) {
                badPath += 1;
+               g.setColor(Color.WHITE);
             }
-            else if (newLine.intersectsLine(openingLine)) {
+            else if (newLine.intersectsLine(openingLine) && badPath == 0) {
                 goodPath += 1;
                 g.setColor(Color.RED);
             }
+            if (goodPath >= 1 && newLine.intersectsLine(backLine) && slope > 0){
+                goodGoodPath += 1;
+                g.setColor(Color.PINK);
+            }
         }
-        if (goodPath == 0){
+        if (badPath >= 1){
+            g.setColor(Color.WHITE);
+        }
+        else if (goodPath == 0){
             g.setColor(Color.WHITE);
         }
     }
 
     public void draw(Graphics2D g) {
-        for (int k = 0; k < numCurves; k++) {
-            drawOneCurve(g);
-            g.setColor(Color.WHITE);
-            if (goodPath >= 1){
-                thetaList.add(theta); //use if you want middle angle
-                //bestTheta = theta; //use if you want max angle
+        for (int i = 0; i < numVelocities; i++) {
+            for (int k = 0; k < numCurves; k++) {
+                drawOneCurve(g);
+                g.setColor(Color.WHITE);
+                //if (goodPath >= 1) {
+                    //thetaList.add(theta); //use if you want middle angle
+                    //bestTheta = theta; //use if you want max angle
+                //}
+                resetSetup();
+                theta += delta_theta;
             }
-            resetSetup();
-            theta += delta_theta;
+            if (goodGoodPath >= 1){
+                velocityList.add(velocity);
+            }
+            goodGoodPath = 0;
+            velocity += delta_velocity;
         }
-
 
         g.setColor(Color.GREEN);
         g.draw(backSpeaker); //vertical wall
@@ -168,17 +205,40 @@ public class Simulation {
         g.draw(frontSpeaker); //shorter vertical wall
         g.setColor(Color.ORANGE);
         g.draw(openingLine); //opening line
+        g.setColor(Color.CYAN);
         g.draw(backLine);
 
-        double sum = 0;
-        for (Double aDouble : thetaList) {
-            sum += aDouble;
+        double sumVelocity = 0;
+        for (Double aDouble : velocityList) {
+            sumVelocity += aDouble;
         }
-        //System.out.println("Best theta: " + bestTheta);
-        System.out.println("Best theta: " + sum/thetaList.size());
-        System.out.println("Current velocity: " + velocity);
-        System.out.println("run!");
+        bestVelocity = sumVelocity/velocityList.size();
+
+        velocity = bestVelocity;
+        theta = initialTheta;
+        for (int k = 0; k < numCurves; k++) {
+            drawOneCurve(g);
+            g.setColor(Color.MAGENTA);
+            if (goodPath >= 1) {
+                thetaList.add(theta); //use if you want middle angle
+                //bestTheta = theta; //use if you want max angle
+            }
+            resetSetup();
+            theta += delta_theta;
+        }
+        double sumTheta = 0;
+        for (Double add : thetaList) {
+            sumTheta += add;
+        }
+        bestTheta = sumTheta/thetaList.size();
+
+        System.out.println("Best theta: " + bestTheta);
+        System.out.println("Best velocity: " + bestVelocity);
+        //System.out.println("Best theta: " + sum/thetaList.size());
+        //System.out.println("Current velocity: " + velocity);
+        //System.out.println("run!");
+
+        //stupid fix to solve bug with it constantly running the same thing
+        setup();
     }
-
-
 }
